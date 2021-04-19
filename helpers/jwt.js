da6e741881f3f4b466a8ff71bfc346d16902fc29
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-04-19 21:07:24 +0700

    Add

---
+++ helpers/jwt.js
@@ -0,0 +1,32 @@
+// JSON WEB TOKEN 
+// Check token 
+const expressJwt = require('express-jwt');
+
+function authJwt() {
+    const secret = process.env.secret;
+    const api = process.env.API_URL;
+    return expressJwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked //User Role
+    }).unless({
+        path: [
+            {url: /\/public\/uploads(.*)/  , methods: ['GET','OPTIONS'] },
+            {url: /\/api\/v1\/products(.*)/  , methods: ['GET','OPTIONS'] },
+            {url: /\/api\/v1\/categories(.*)/  , methods: ['GET','OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
+            `${api}/users/changePassword`,
+            `${api}/users/updateUser`,
+            `${api}/users/getCurrentUser`,
+        ]
+    })
+}
+
+async function isRevoked(req, payLoad, done){
+    if(!payLoad.isAdmin){
+        done(null, true)
+    }
+    done();
+}
+module.exports = authJwt;
